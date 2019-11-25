8642af012c0ce95426fc6dc86e5fe77ef23a61e2
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2019-11-25 21:58:02 +0200

    features: background image, sass enabled and compiled files, html

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// j querry toggle class for menu
+
+// $('.menu').on('click', function () {
+//     $(this).toggleClass('active');
+//     $('.overlay').toggleClass('menu-open');
+//   });
+  
+//   $('.nav a').on('click', function () {
+//     $('.menu').removeClass('active');
+//     $('.overlay').removeClass('menu-open');
+//   });

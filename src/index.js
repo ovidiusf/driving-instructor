6488aca5474526584ef8a8e5f79bb2e1f7e1bc17
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2019-11-26 20:20:47 +0200

    made navigation bar responsive

--- src/index.js
+++ src/index.js
@@ -9,3 +9,12 @@
 //     $('.menu').removeClass('active');
 //     $('.overlay').removeClass('menu-open');
 //   });
+
+
+$(document).ready(function() {
+    $(".burger-menu").click(function() {
+      $(".stick").toggleClass(function () {
+        return $(this).is('.open, .close') ? 'open close' : 'open';
+      });
+    });
+  });

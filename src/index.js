65d876ac3fd36c3004e5ec72a9062972487c5dd2
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2019-11-27 00:05:31 +0200

    responsive navbar with animation

--- src/index.js
+++ src/index.js
@@ -10,10 +10,32 @@ $(document).ready(function () {
 });
 
 // enables menu to toggle down
-let mainNav = document.querySelector('#main-nav');
-let navBarToggle = document.querySelector('#burger-toggle');
+const mainNav = document.querySelector('#main-list');
+const nav = document.querySelector('#main-nav');
+const navBarToggle = document.querySelector('#burger-toggle');
+const header = document.querySelector('#home-header');
+
 console.log(mainNav.classList);
-navBarToggle.addEventListener('click', function() {
-    // console.log('active');
+navBarToggle.addEventListener('click', function () {
+
+    header.classList.toggle('active');
     mainNav.classList.toggle('active');
+
 });
+
+// toggle elements
+
+// $(".burger-menu").click(function () {
+//     if($(".menu-list").hasClass("closed")){
+//         $(".menu-list").removeClass("closed");
+//       $(".menu-list").animate({
+//         top: '-600px'
+//       }, 1000);
+      
+//     }else{
+//       $(".menu-list").animate({
+//         top: '0px'
+//       }, 1000);
+//       $(".menu-list").addClass("closed");
+//     }
+//   });

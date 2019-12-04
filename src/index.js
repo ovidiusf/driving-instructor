79e3ec6874d0a37c38eaca7d11460a57f364a9ef
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2019-12-04 13:39:43 +0200

    fixed white line with js file

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const burgerMenuStickComponents = document.getElementsByClassName('stick');
 const listItemsNav = document.querySelectorAll("#main-nav > ul > li");
 let prevScrollpos = window.pageYOffset;
 const windowCurrentWidth = 900;
-const headerHeight = "-8.1rem";
+const headerHeight = "-8rem";
 const returnToTopButton = document.querySelector('#return-to-top');
 
 // changes behaviour of the navigation bar when mobile
